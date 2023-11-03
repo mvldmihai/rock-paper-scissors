2c842d3b580576b2b851d41f2349a49c6d2a9866
Bertrandson <118061867+mvldmihai@users.noreply.github.com>
2023-11-03 13:45:42 +0200

    Add game logic with playRound function

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
-        return "Rock";
+        return "rock";
     } else if (choice === 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
+    }
+}
+
+// const playerSelection = prompt("Enter your choice: ").toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        return "It's a tie";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "It's a tie";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper";
+    } else {
+        return "It's a tie"
     }
 }
